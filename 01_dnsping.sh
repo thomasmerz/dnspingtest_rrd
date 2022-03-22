11aaf666c196a6adf6aafadfce52a10b5406a8e5
Thomas Merz <git@rhoenwurz.de>
2022-03-22 22:46:51 +0100

    ✨ add options: 01_dnsping.sh [random|example.com] 🚸 use opendns-domains from local files for random domains 📦 add examples for random domains

--- 01_dnsping.sh
+++ 01_dnsping.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Usage: 01_dnsping.sh [random|example.com]
+
 cd ~/dev/dnspingtest_rrd/ || exit 1
 
 PING=/usr/bin/dnsping
@@ -9,17 +11,34 @@ DEADLINE=1  # even 1s (1000ms) is much too long to wait for!
 # ---
 # inspired by: https://github.com/gitthangbaby/dnsperftest/blob/patch-1/dnstest_random#L28-L35
 # ---
-# Random domains to choose from
-mapfile -t RANDOM_DOMAINS < <(
-curl -sS https://raw.githubusercontent.com/opendns/public-domain-lists/master/opendns-top-domains.txt
-curl -sS https://raw.githubusercontent.com/opendns/public-domain-lists/master/opendns-random-domains.txt
-)
+if [ "$1" = "random" ]; then
+  random=true
+  COUNT=10
+  # Random domains to choose from
+  mapfile -t RANDOM_DOMAINS < <(
+  cat opendns-random-domains.txt
+  cat opendns-top-domains.txt
+# Info:
+# "Both lists are in public domain, and are updated weekly"
+# But: last update 8 years ago :-(
+# So there's no need to download these lists again and again.
+# I opened an issue at Cisco Umbrella (fka OpenDNS) to reactivate updating these lists.
+#  curl -sS https://raw.githubusercontent.com/opendns/public-domain-lists/master/opendns-top-domains.txt
+#  curl -sS https://raw.githubusercontent.com/opendns/public-domain-lists/master/opendns-random-domains.txt
+  )
+else
+  domain="$1"
+fi
 # ---
 
 dnsping_host() {
-    random_domain=${RANDOM_DOMAINS[$RANDOM % ${#RANDOM_DOMAINS[*]}]};
-    echo "querying $random_domain for $1"
-    output="$($PING -q -c $COUNT -w $DEADLINE -s "$1" "$random_domain" 2>&1)"
+    if [ "$random" = "true" ]; then
+      domain=${RANDOM_DOMAINS[$RANDOM % ${#RANDOM_DOMAINS[*]}]};
+    else
+      domain=${domain:=heise.de}
+    fi
+    echo "querying $domain for $1"
+    output="$($PING -q -c $COUNT -w $DEADLINE -s "$1" "$domain" 2>&1)"
     # notice $output is quoted to preserve newlines
     temp=$(echo "$output"| awk '
         BEGIN           {pl=100; rtt=0.1}
@@ -40,6 +59,7 @@ dnsping_host() {
         END         {print pl ":" rtt}
         '|cut -d"=" -f2)
     #local temp
+    echo -e "loss in percent : avg query time in ms => $temp\n"
     RETURN_VALUE=$temp
 }
 
